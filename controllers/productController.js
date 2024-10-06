0360f01cbb7d381c7de90f498bca7b3aa07eec52
iamyashdp <iamyashdp@outlook.com>
2024-10-06 13:01:54 -0400

    Functional product search by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,13 +14,35 @@ const createProduct = async (req, res) => {
 // Get all products from mongodb
 const getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    products = await Product.find();
     res.status(200).send(products);
   } catch (err) {
+    res.status(500).send(err);
+  }
+
+};
+
+// Get products by ID
+const getProductsByID = async (req, res) => {
+  try {
+    const productID = req.params.id;
+    const product = await Product.findById(productID);
+
+    if (!product) {
+      return res.status(404).send({ error: "product not found" });
+    }
+
+    res.status(200).send(product);
+  } catch (err) {
+    if (err.name === 'CastError') {
+      return res.status(404).send({ error: "product not found" });
+    }
+
     res.status(500).send(err);
   }
 };
 
 
 
-module.exports = { createProduct, getAllProducts };
+
+module.exports = { createProduct, getAllProducts, getProductsByID };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createProduct, getAllProducts } = require('../controllers/productController');
+const { createProduct, getAllProducts, getProductsByID } = require('../controllers/productController');
 const router = express.Router();
 
 // Define the routes for creating and fetching products
 router.post('/products', createProduct);
 router.get('/products', getAllProducts);
+router.get('/products/:id', getProductsByID);
 
 
 module.exports = router;
